7fdf2534e392f01729576f9635d2aebc9cd871f8
sediri-code <63328434+sediri-code@users.noreply.github.com>
2023-09-18 13:55:47 +0200

    updated project page

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -2,12 +2,13 @@ import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
-import leaf from "../../Assets/Projects/leaf.png";
-import emotion from "../../Assets/Projects/emotion.png";
-import editor from "../../Assets/Projects/codeEditor.png";
-import chatify from "../../Assets/Projects/chatify.png";
-import suicide from "../../Assets/Projects/suicide.png";
-import bitsOfCode from "../../Assets/Projects/blog.png";
+
+import netmax from "../../Assets/Projects/netmax.jpg"
+import churn from "../../Assets/Projects/churn.jpg"
+import canadin from "../../Assets/Projects/canadin.jpg"
+import codeshare from "../../Assets/Projects/codeshare.jpg"
+import dashboard from "../../Assets/Projects/dashboard.jpg"
+import bottrade from "../../Assets/Projects/bottrade.jpg"
 
 function Projects() {
   return (
@@ -23,67 +24,66 @@ function Projects() {
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={chatify}
+              imgPath={netmax}
               isBlog={false}
-              title="Chatify"
-              description="Personal Chat Room or Workspace to share resources and hangout with friends build with react.js, Material-UI, and Firebase. Have features which allows user for realtime messaging, image sharing as well as supports reactions on messages."
-              ghLink="https://github.com/soumyajit4419/Chatify"
-              demoLink="https://chatify-49.web.app/"
+              title="Netmax"
+              description="Netmax est un site web qui aide les gens à développer leurs comptes sur les réseaux sociaux tels que TikTok et Instagram. Il a été construit en utilisant Astro JS, Supabase (PostgreSQL) et Notion. Dans ce projet, j'ai appris sur le rendu côté serveur (SSR), les certificats SSL,Preline UI et l'intégration des API et j'ai amélioré mes connaissances en JavaScript."
+              demoLink="https://netmax.live"
             />
           </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={bitsOfCode}
+              imgPath={churn}
               isBlog={false}
-              title="Bits-0f-C0de"
-              description="My personal blog page build with Next.js and Tailwind Css which takes the content from makdown files and renders it using Next.js. Supports dark mode and easy to write blogs using markdown."
-              ghLink="https://github.com/soumyajit4419/Bits-0f-C0de"
-              demoLink="https://blogs.soumya-jit.tech/"
+              title="Banking Churn"
+              description="Ce projet consiste à prédire si le client va rester fidèle à sa banque au cours des prochaines années ou non. Ici, j'ai appris à réaliser une analyse discriminante.."
+              ghLink="https://github.com/sediri-code/banking_churn"
+              demoLink="https://github.com/sediri-code/banking_churn/blob/main/churn_prediction.ipynb"
             />
           </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={editor}
+              imgPath={canadin}
               isBlog={false}
-              title="Editor.io"
-              description="Online code and markdown editor build with react.js. Online Editor which supports html, css, and js code with instant view of website. Online markdown editor for building README file which supports GFM, Custom Html tags with toolbar and instant preview.Both the editor supports auto save of work using Local Storage"
-              ghLink="https://github.com/soumyajit4419/Editor.io"
-              demoLink="https://editor.soumya-jit.tech/"              
+              title="Canadin App"
+              description="
+              Pendant mon stage chez Canadin App, j'ai amélioré mes compétences en React JS en développant la page d'authentification du site web."
+              
+              demoLink="https://canadin-app.com/"              
             />
           </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={leaf}
+              imgPath={codeshare}
               isBlog={false}
-              title="Plant AI"
-              description="Used the plant disease dataset from Kaggle and trained a image classifer model using 'PyTorch' framework using CNN and Transfer Learning with 38 classes of various plant leaves. The model was successfully able to detect diseased and healthy leaves of 14 unique plants. I was able to achieve an accuracy of 98% by using Resnet34 pretrained model."
-              ghLink="https://github.com/soumyajit4419/Plant_AI"
-              demoLink="https://plant49-ai.herokuapp.com/"
+              title="Code share"
+              description="J'ai créé une mini plateforme qui facilite le partage de code entre les développeurs. J'ai utilisé Svelte dans le but de l'apprendre."
+              ghLink="https://github.com/sediri-code/CodeSync"
+              demoLink="https://inquisitive-begonia-d62e99.netlify.app/"
             />
           </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={suicide}
+              imgPath={dashboard}
               isBlog={false}
-              title="Ai For Social Good"
-              description="Using 'Natural Launguage Processing' for the detection of suicide-related posts and user's suicide ideation in cyberspace  and thus helping in sucide prevention."
-              ghLink="https://github.com/soumyajit4419/AI_For_Social_Good"
+              title="Sales Dashboard"
+              description="J'ai développé un tableau de bord de ventes pour les données d'un site e-commerce en utilisant l'outil Power BI, dans le but d'améliorer la qualité des acheteurs ciblés."
+             
               // demoLink="https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley" <--------Please include a demo link here
             />
           </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={emotion}
+              imgPath={bottrade}
               isBlog={false}
-              title="Face Recognition and Emotion Detection"
-              description="Trained a CNN classifier using 'FER-2013 dataset' with Keras and tensorflow backened. The classifier sucessfully predicted the various types of emotions of human. And the highest accuracy obtained with the model was 60.1%.
-              Then used Open-CV to detect the face in an image and then pass the face to the classifer to predict the emotion of a person."
-              ghLink="https://github.com/soumyajit4419/Face_And_Emotion_Detection"
+              title="Trade bot"
+              description="J'ai créé un bot qui achète et vend des positions sur le programme Meta 4 Teletrade avec un taux de réussite de 70 % en utilisant le langage MQL4."
+              ghLink="https://github.com/sediri-code/MQL4-trade-bot"
               // demoLink="https://blogs.soumya-jit.tech/"      <--------Please include a demo link here 
             />
           </Col>
